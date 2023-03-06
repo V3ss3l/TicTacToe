7b57175dd8185bbb0f59e088f750b3094e0c5a49
V3ss3l <grishav3ss3l@gmail.com>
2023-03-06 10:08:05 +0400

    Добавьте файлы проекта.

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label_Message = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label_9 = new System.Windows.Forms.Label();
+            this.label_8 = new System.Windows.Forms.Label();
+            this.label_7 = new System.Windows.Forms.Label();
+            this.label_6 = new System.Windows.Forms.Label();
+            this.label_5 = new System.Windows.Forms.Label();
+            this.label_4 = new System.Windows.Forms.Label();
+            this.label_3 = new System.Windows.Forms.Label();
+            this.label_2 = new System.Windows.Forms.Label();
+            this.label_1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(555, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(233, 128);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Настройки игры";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(7, 43);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(145, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Первый - пользователь";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(131, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Первый - компьютер";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Играть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label_Message
+            // 
+            this.label_Message.AutoSize = true;
+            this.label_Message.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_Message.Location = new System.Drawing.Point(550, 367);
+            this.label_Message.Name = "label_Message";
+            this.label_Message.Size = new System.Drawing.Size(176, 29);
+            this.label_Message.TabIndex = 2;
+            this.label_Message.Text = "Оповещения";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.label_9, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label_8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label_7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label_6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label_5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label_4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label_3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label_2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label_1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 21);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(532, 426);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // label_9
+            // 
+            this.label_9.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_9.AutoSize = true;
+            this.label_9.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_9.Location = new System.Drawing.Point(358, 283);
+            this.label_9.Name = "label_9";
+            this.label_9.Size = new System.Drawing.Size(170, 142);
+            this.label_9.TabIndex = 8;
+            this.label_9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label_8
+            // 
+            this.label_8.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_8.AutoSize = true;
+            this.label_8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_8.Location = new System.Drawing.Point(181, 283);
+            this.label_8.Name = "label_8";
+            this.label_8.Size = new System.Drawing.Size(170, 142);
+            this.label_8.TabIndex = 7;
+            this.label_8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label_7
+            // 
+            this.label_7.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_7.AutoSize = true;
+            this.label_7.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_7.Location = new System.Drawing.Point(4, 283);
+            this.label_7.Name = "label_7";
+            this.label_7.Size = new System.Drawing.Size(170, 142);
+            this.label_7.TabIndex = 6;
+            this.label_7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label_6
+            // 
+            this.label_6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_6.AutoSize = true;
+            this.label_6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_6.Location = new System.Drawing.Point(358, 142);
+            this.label_6.Name = "label_6";
+            this.label_6.Size = new System.Drawing.Size(170, 140);
+            this.label_6.TabIndex = 5;
+            this.label_6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label_5
+            // 
+            this.label_5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_5.AutoSize = true;
+            this.label_5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_5.Location = new System.Drawing.Point(181, 142);
+            this.label_5.Name = "label_5";
+            this.label_5.Size = new System.Drawing.Size(170, 140);
+            this.label_5.TabIndex = 4;
+            this.label_5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label_4
+            // 
+            this.label_4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_4.AutoSize = true;
+            this.label_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_4.Location = new System.Drawing.Point(4, 142);
+            this.label_4.Name = "label_4";
+            this.label_4.Size = new System.Drawing.Size(170, 140);
+            this.label_4.TabIndex = 3;
+            this.label_4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label_3
+            // 
+            this.label_3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_3.AutoSize = true;
+            this.label_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_3.Location = new System.Drawing.Point(358, 1);
+            this.label_3.Name = "label_3";
+            this.label_3.Size = new System.Drawing.Size(170, 140);
+            this.label_3.TabIndex = 2;
+            this.label_3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label_2
+            // 
+            this.label_2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_2.AutoSize = true;
+            this.label_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_2.Location = new System.Drawing.Point(181, 1);
+            this.label_2.Name = "label_2";
+            this.label_2.Size = new System.Drawing.Size(170, 140);
+            this.label_2.TabIndex = 1;
+            this.label_2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label_1
+            // 
+            this.label_1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_1.AutoSize = true;
+            this.label_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_1.Location = new System.Drawing.Point(4, 1);
+            this.label_1.Name = "label_1";
+            this.label_1.Size = new System.Drawing.Size(170, 140);
+            this.label_1.TabIndex = 0;
+            this.label_1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 467);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label_Message);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label_Message;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label_9;
+        private System.Windows.Forms.Label label_8;
+        private System.Windows.Forms.Label label_7;
+        private System.Windows.Forms.Label label_6;
+        private System.Windows.Forms.Label label_5;
+        private System.Windows.Forms.Label label_4;
+        private System.Windows.Forms.Label label_3;
+        private System.Windows.Forms.Label label_2;
+        private System.Windows.Forms.Label label_1;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        private bool _firstPlayer = true;
+        private bool isMoveFinished = false;
+        private int[,] _gameBoard;
+        public Form1()
+        {
+            InitializeComponent();
+            _gameBoard = new int[,]
+            {
+                { 0, 0, 0 },
+                { 0, 0, 0 },
+                { 0, 0, 0}
+            };
+            foreach (Label i in tableLayoutPanel1.Controls)
+            { 
+                i.Click += new EventHandler(label_Click);
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            _firstPlayer = false;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            _firstPlayer = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!_firstPlayer)
+            {
+                _gameBoard[2, 2] = 2;
+                label_5.Text = "X";
+                label_Message.Text = "Теперь ваш ход!";
+            }
+            else
+            {
+                label_Message.Text = "Ваш ход первый!";
+            }
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+            Label buff = (Label)sender;
+            if(_firstPlayer)
+            {
+                buff.Text = "X";
+                pcMove()
+            }
+            else
+            {
+                buff.Text = "X";
+            }
+        }
+
+        private void pcMove()
+        {
+
+        }
+    }
+}
